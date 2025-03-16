c395d5c6b8fb686f1332db42876c743a09b70e62
HenryVC08 <henryvaldezcusi.08@gmail.com>
2025-03-16 18:25:43 -0500

    Eliminar elementos

---
+++ js/03-app.js
@@ -0,0 +1,14 @@
+//Eliminar elementos 
+
+localStorage.removeItem('nombre')
+
+
+//Actualizar elemento
+
+const mesesArray = JSON.parse(localStorage.getItem('meses'))
+console.log(mesesArray)
+
+mesesArray.push('Nuevo mes')
+console.log(mesesArray)
+
+localStorage.setItem('meses',JSON.stringify(mesesArray))
